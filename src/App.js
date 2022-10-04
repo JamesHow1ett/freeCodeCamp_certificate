c67267c2436419cdec8f386edb4ce243f68a3509
JamesHow1ett <mopis101@gmail.com>
2022-10-04 20:53:16 +0300

    add default md

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { useState } from "react";
 import TextContainer from "./components/TextContainer";
 
+const blockCode = `
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+`;
+
+const listMd = `
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+`;
+
+const defaultMd = [
+  "# H1",
+  "## H2",
+  "Heres some code, `<div></div>`, between 2 backticks.",
+  blockCode,
+  "There's also [links](https://www.freecodecamp.org), and",
+  "You can also make text **bold**... whoa!",
+  "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)",
+  listMd,
+  "> Block Quotes!",
+].join("\n");
+
 function App() {
-  const [text, setText] = useState("# Test");
+  const [text, setText] = useState(defaultMd);
 
   const handleSetText = (e) => {
     setText(e.target.value);
