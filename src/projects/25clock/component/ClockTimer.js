244552a2a527d6a0181a5f86a071fcc9a1e94ecb
JamesHow1ett <mopis101@gmail.com>
2022-10-31 15:34:01 +0200

    replace .css file to tailwind classes

--- src/projects/25clock/component/ClockTimer.js
+++ src/projects/25clock/component/ClockTimer.js
@@ -21,17 +21,18 @@ import {
 } from "../store/timerReducer";
 
 import beep from "../assets/beep.ogg";
-import "../styles/ClockTimer.css";
 
-const ArrowDownIcon = <FontAwesomeIcon icon={faArrowDown} size="2x" />;
-const ArrowUpIcon = <FontAwesomeIcon icon={faArrowUp} size="2x" />;
+const ICON_SIZE = "lg";
+
+const ArrowDownIcon = <FontAwesomeIcon icon={faArrowDown} size={ICON_SIZE} />;
+const ArrowUpIcon = <FontAwesomeIcon icon={faArrowUp} size={ICON_SIZE} />;
 const StartStopIcon = (
   <>
-    <FontAwesomeIcon icon={faPlay} size="2x" />
-    <FontAwesomeIcon icon={faPause} size="2x" />
+    <FontAwesomeIcon icon={faPlay} size={ICON_SIZE} />
+    <FontAwesomeIcon icon={faPause} size={ICON_SIZE} />
   </>
 );
-const ResetIcon = <FontAwesomeIcon icon={faRefresh} size="2x" />;
+const ResetIcon = <FontAwesomeIcon icon={faRefresh} size={ICON_SIZE} />;
 
 function ClockTimer() {
   const {
@@ -87,48 +88,50 @@ function ClockTimer() {
   const isLessThanMinute = (isBreakStarted || isSessionStarted) && timeLeft.minutes <= 0;
 
   return (
-    <div id="clock-app" className="container">
-      <div>
-        <div className="main-title">25 + 5 Clock</div>
-        <div className="length-controls-wrapper">
-          <div className="length-control">
-            <div id="break-label">Break Length</div>
+    <div className="w-full h-screen flex flex-col items-center bg-[#1e555c] text-[30px] text-white text-center">
+      <div className="font-bold text-4xl">25 + 5 Clock</div>
+      <div className="flex gap-[50px] mt-10 mb-10">
+        <div className="flex flex-col gap-5">
+          <div id="break-label">Break Length</div>
+          <div className="flex items-center justify-center gap-4">
             <button
               id="break-decrement"
-              className="btn-level"
+              className="hover:text-teal-400 transition ease-in-out"
               type="button"
               onClick={decrementBreak}
             >
               {ArrowDownIcon}
             </button>
-            <div className="btn-level" id="break-length">
+            <div className="min-w-[35px]" id="break-length">
               {breakLength}
             </div>
             <button
               id="break-increment"
-              className="btn-level"
+              className="hover:text-teal-400 transition ease-in-out"
               type="button"
               onClick={incrementBreak}
             >
               {ArrowUpIcon}
             </button>
           </div>
-          <div className="length-control">
-            <div id="session-label">Session Length</div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div id="session-label">Session Length</div>
+          <div className="flex items-center justify-center gap-4">
             <button
               id="session-decrement"
-              className="btn-level"
+              className="hover:text-teal-400 transition ease-in-out"
               type="button"
               onClick={decrementSession}
             >
               {ArrowDownIcon}
             </button>
-            <div className="btn-level" id="session-length">
+            <div className="min-w-[35px]" id="session-length">
               {sessionLength}
             </div>
             <button
               id="session-increment"
-              className="btn-level"
+              className="hover:text-teal-400 transition ease-in-out"
               type="button"
               onClick={incrementSession}
             >
@@ -136,21 +139,37 @@ function ClockTimer() {
             </button>
           </div>
         </div>
-        <div className="timer">
-          <div className="timer-wrapper" style={{ color: isLessThanMinute ? "#a50d0d" : "#fff" }}>
-            <div id="timer-label">{timerTitle}</div>
-            <div id="time-left">{timeLeftParsed}</div>
+      </div>
+      <div className="pl-10 pr-10 pt-5 pb-5 mt-5 mb-3 border-8 border-emerald-900 rounded-[50px]">
+        <div
+          className="flex flex-col gap-5"
+          style={{ color: isLessThanMinute ? "#a50d0d" : "#fff" }}
+        >
+          <div id="timer-label">{timerTitle}</div>
+          <div id="time-left" className="font-[digital] text-[45px] normal-nums">
+            {timeLeftParsed}
           </div>
         </div>
-        <div className="timer-control">
-          <button id="start_stop" type="button" onClick={startStop}>
-            {StartStopIcon}
-          </button>
-          <button id="reset" type="button" onClick={reset}>
-            {ResetIcon}
-          </button>
-        </div>
       </div>
+      <div className="flex gap-3">
+        <button
+          id="start_stop"
+          className="hover:text-teal-400 transition ease-in-out"
+          type="button"
+          onClick={startStop}
+        >
+          {StartStopIcon}
+        </button>
+        <button
+          id="reset"
+          className="hover:text-teal-400 transition ease-in-out"
+          type="button"
+          onClick={reset}
+        >
+          {ResetIcon}
+        </button>
+      </div>
+
       <audio id="beep" ref={audioRef}>
         <source src={beep} type="audio/ogg" />
       </audio>
