62da42084e8a2566e0477e1500b07d2a1d60faac
JamesHow1ett <mopis101@gmail.com>
2022-11-01 10:28:33 +0200

    merge main

--- src/projects/drum/store/index.js
+++ src/projects/drum/store/index.js
@@ -1,9 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD:src/projects/drum/store/index.js
 import drumReducer from "./drumReducer";
-=======
-import timerReducer from "./timerReducer";
->>>>>>> main:src/projects/25clock/store/index.js
 
 export const store = configureStore({
   reducer: {
